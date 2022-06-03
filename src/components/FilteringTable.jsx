dbfa2e8691f7d09b5d727cd84aa99cb391c1da3d
juanfranciscogco <juanfranciscogco@gmail.com>
2022-06-03 18:16:21 -0500

    Filters, Paginations

--- src/components/FilteringTable.jsx
+++ src/components/FilteringTable.jsx
@@ -1,13 +1,20 @@
 import React, { useMemo } from "react";
-import { useTable, useGlobalFilter } from "react-table";
+import { useTable, useGlobalFilter, useFilters } from "react-table";
 import MOCK_DATA from "./MOCK_DATA.json";
 import { COLUMNS, GROUPED_COLUMNS } from "./columns";
 import "./table.css";
 import { GlobalFilter } from "./GlobalFilter";
+import { ColumnFilter } from "./ColumnFilter";
 
 const FilteringTable = () => {
   const columns = useMemo(() => COLUMNS, []);
   const data = useMemo(() => MOCK_DATA, []);
+
+  const defaultColumn = useMemo(() => {
+    return {
+      Filter: ColumnFilter
+    }
+  },[])
 
   const {
     getTableProps,
@@ -22,7 +29,9 @@ const FilteringTable = () => {
     {
       columns,
       data,
+      defaultColumn
     },
+    useFilters,
     useGlobalFilter
   );
 
@@ -36,7 +45,10 @@ const FilteringTable = () => {
           {headerGroups.map((headerGroup) => (
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
-                <th {...column.getHeaderProps()}>{column.render("Header")}</th>
+                <th {...column.getHeaderProps()}>
+                  {column.render("Header")}
+                  <div>{column.canFilter ? column.render("Filter") : null}</div>
+                </th>
               ))}
             </tr>
           ))}
