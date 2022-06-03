0a94cc7f67ea9d3a702f1e05808fc79c66f007b0
juanfranciscogco <juanfranciscogco@gmail.com>
2022-06-03 18:15:24 -0500

    Filters, Pagination

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import BasicTable from "./components/BasicTable";
 import FilteringTable from "./components/FilteringTable";
+import PaginationTable from "./components/PaginationTable";
+import RowSelection from "./components/RowSelection";
 import SortingTable from "./components/SortingTable";
 
 function App() {
   return (
     <div className="App">
-      <FilteringTable />
+      <RowSelection />
     </div>
   );
 }
