505cac688fa0b4c4ff8dc505fead8216c2ad8722
juanfranciscogco <juanfranciscogco@gmail.com>
2022-06-08 13:11:31 -0500

    Column Order

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import BasicTable from "./components/BasicTable";
+import ColumnOrder from "./components/ColumnOrder";
 import FilteringTable from "./components/FilteringTable";
 import PaginationTable from "./components/PaginationTable";
 import RowSelection from "./components/RowSelection";
@@ -8,7 +9,7 @@ import SortingTable from "./components/SortingTable";
 function App() {
   return (
     <div>
-      <RowSelection />
+      <ColumnOrder />
     </div>
   );
 }
